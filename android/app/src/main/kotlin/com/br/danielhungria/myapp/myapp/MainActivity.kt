bd3446a064957e67f1b9ae6ac38da18e8e7b49dc
Daniel Hungria <danielhungria14@gmail.com>
2023-01-31 11:26:11 -0300

    upload

---
+++ android/app/src/main/kotlin/com/br/danielhungria/myapp/myapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.br.danielhungria.myapp.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../util/todo_tile.dart';
+import '../util/dialog_box.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+
+  //list of todo task
+  List toDoList = [
+    ["Make tutorial", true],
+    ["Do exercise", false],
+  ];
+
+  //checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: const Text('To Do'),
+        backgroundColor: Colors.yellow,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return ToDoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            );
+          }),
+    );
+  }
+}
